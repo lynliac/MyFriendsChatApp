828a8d88bdcabca0195ab3bf7860dfd7b41ed5cc
lynliac <lynliac@dunwoody.edu>
2020-01-14 12:45:47 -0600

    started with login.php file and adding basic structure to website

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login To Your Account</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="login-form">
+		<form action="" method="">
+			<div class="form-header">
+				<h2>Login</h2>
+				<p>Login to MyFriends</p>
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input type="email" class="form-control" name="email" placeholder="someone@example.com" autocomplete="off" required>
+			</div>
+			<div class="form-group">
+				<label>Password</label>
+				<input type="password" class="form-control" name="pass" placeholder="Password" autocomplete="off" required>
+			</div>
+			<div class="small">Forgot password? <a href="forgot_pass.php">Click Here</a></div><br>
+		</form>
+	</div>
+	
+</body>
+</html>
